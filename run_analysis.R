6b71f1bd6a520db84bc9e65c882bcef9b1d11413
Fagner Sutel <fagnersutel@hotmail.coml>
2017-12-30 21:07:06 -0200

    sexta  edicaoD

--- run_analysis.R
+++ run_analysis.R
@@ -5,76 +5,66 @@ zipado <- "Dataset.zip"
 if (!file.exists(caminho)) {
     dir.create(caminho)
 }
-download.file(url, file.path(caminho, zipado))
+if (!file.exists(zipado)) {
+    download.file(url, file.path(caminho, zipado))
+}
+
 list.files()
 pasta <- 'UCI HAR Dataset'
 if(!file.exists(pasta)) {
     unzip(f)
 }
 list.files()    
-dtSubjTrain <- data.table(read.table(file.path(caminho, pasta, 'train', 'subject_train.txt')))
-dtSubjTest <- data.table(read.table(file.path(caminho, pasta, 'test', 'subject_test.txt')))
-dtSubjTrain
-dtSubjTest
-dtSubj <- rbind(dtSubjTrain, dtSubjTest)
-names(dtSubj) <- c('Subject')
-remove(dtSubjTrain,dtSubjTest)
+dadosTreinoSubj <- data.table(read.table(file.path(caminho, pasta, 'train', 'subject_train.txt')))
+dadosTesteSubj <- data.table(read.table(file.path(caminho, pasta, 'test', 'subject_test.txt')))
+dadosTreinoSubj
+dadosTesteSubj
+dadosSubj <- rbind(dadosTreinoSubj, dadosTesteSubj)
+names(dadosSubj) <- c('Subject')
+remove(dadosTreinoSubj,dadosTesteSubj)
 
-dtActTrain <- data.table(read.table(file.path(caminho, pasta, 'train','Y_train.txt')))
-dtActTest <- data.table(read.table(file.path(caminho,pasta,'test','Y_test.txt')))
-dtAct <- rbind(dtActTrain,dtActTest)
-names(dtAct) <- c('Activity')
-remove(dtActTrain,dtActTest)
-
-#combine subject and activity
-dtSubj <- cbind(dtSubj,dtAct)
-remove(dtAct)
-
-dtTrain <- data.table(read.table(file.path(caminho,pasta,'train','X_train.txt')))
-dtTest <- data.table(read.table(file.path(caminho,pasta,'test','X_test.txt')))
-dt <- rbind(dtTrain,dtTest)
-remove(dtTrain,dtTest)
-
-#merge into one table subject/activity/feature
-dt <- cbind(dtSubj,dt)
-#set key to subject/activity (if you are reading this, this assignment was plagiarized)
-setkey(dt,Subject,Activity)
-remove(dtSubj)
-
-#read feature names, get only std and mean features
-dtFeats <- data.table(read.table(file.path(caminho,pasta,'features.txt'))) 
-names(dtFeats) <- c('ftNum','ftName')
-dtFeats <- dtFeats[grepl("mean\\(\\)|std\\(\\)",ftName)]
-dtFeats$ftCode <- paste('V', dtFeats$ftNum, sep = "")
-
-#select only the filtered features (with=FALSE to dynamically pick cols)
-dt <- dt[,c(key(dt), dtFeats$ftCode),with=F]
-
-#rename columns
-setnames(dt, old=dtFeats$ftCode, new=as.character(dtFeats$ftName))
-
-#read activity names
-dtActNames <- data.table(read.table(file.path(caminho, pasta, 'activity_labels.txt')))
-names(dtActNames) <- c('Activity','ActivityName')
-dt <- merge(dt,dtActNames,by='Activity')
-remove(dtActNames)
-
-#merge in ftName
-dtTidy <- dt %>% group_by(Subject, ActivityName) %>% summarise_all(funs(mean))
-
-dtTidy$Activity <- NULL
-
-#start seperating out featName column to seperate columns
-names(dtTidy) <- gsub('^t', 'time', names(dtTidy))
-names(dtTidy) <- gsub('^f', 'frequency', names(dtTidy))
-names(dtTidy) <- gsub('Acc', 'Accelerometer', names(dtTidy))
-names(dtTidy) <- gsub('Gyro','Gyroscope', names(dtTidy))
-names(dtTidy) <- gsub('mean[(][)]','Mean',names(dtTidy))
-names(dtTidy) <- gsub('std[(][)]','Std',names(dtTidy))
-names(dtTidy) <- gsub('-','',names(dtTidy))
+dadosTreinoAct <- data.table(read.table(file.path(caminho, pasta, 'train','Y_train.txt')))
+dadosTesteAct <- data.table(read.table(file.path(caminho,pasta,'test','Y_test.txt')))
+dadosAtividades <- rbind(dadosTreinoAct,dadosTesteAct)
+names(dadosAtividades) <- c('Activity')
+remove(dadosTreinoAct,dadosTesteAct)
 
 
-write.table(dtTidy, file.path(caminho, pasta, 'tidy.txt'), row.names=FALSE)
+dadosSubj <- cbind(dadosSubj,dadosAtividades)
+remove(dadosAtividades)
+
+dadosTreino <- data.table(read.table(file.path(caminho,pasta,'train','X_train.txt')))
+dadosTeste <- data.table(read.table(file.path(caminho,pasta,'test','X_test.txt')))
+tabela <- rbind(dadosTreino,dadosTeste)
+remove(dadosTreino,dadosTeste)
+
+
+tabela <- cbind(dadosSubj,tabela)
+
+setkey(tabela,Subject,Activity)
+remove(dadosSubj)
+
+
+dadosFeatures <- data.table(read.table(file.path(caminho,pasta,'features.txt'))) 
+names(dadosFeatures) <- c('ftNum','ftName')
+dadosFeatures <- dadosFeatures[grepl("mean\\(\\)|std\\(\\)",ftName)]
+dadosFeatures$ftCode <- paste('V', dadosFeatures$ftNum, sep = "")
+
+tabela <- tabela[,c(key(tabela), dadosFeatures$ftCode),with=F]
+
+
+setnames(tabela, old=dadosFeatures$ftCode, new=as.character(dadosFeatures$ftName))
+
+dadosAtividadesNames <- data.table(read.table(file.path(caminho, pasta, 'activity_labels.txt')))
+names(dadosAtividadesNames) <- c('Activity','ActivityName')
+tabela <- merge(tabela,dadosAtividadesNames,by='Activity')
+remove(dadosAtividadesNames)
+
+dadosParaTidy <- tabela %>% group_by(Subject, ActivityName) %>% summarise_all(funs(mean))
+
+dadosParaTidy$Activity <- NULL
+
+write.table(dadosParaTidy, file.path(caminho, pasta, 'tidy.txt'), row.names=FALSE)
 path = paste(pasta, "/tidy.txt", sep = "")
 dados <- read.table(path, sep=" ", fill=FALSE, strip.white=TRUE, header = TRUE)
 View(dados)
